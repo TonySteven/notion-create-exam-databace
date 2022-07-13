27c4f29a2ddfbf04de0a5b13ac013864294ad1e3
StevenL <1012987522@qq.com>
2022-07-13 15:51:22 +0800

    feature: 完成分析json并去重保存到notion数据库中.

--- analysis.py
+++ analysis.py
@@ -8,26 +8,32 @@
 
 import json
 
-from notion_client import Client
+from notion_client import Client as NotionClient
+from redisbloom.client import Client as RedisBloomClient
 
 from utils.utils import stu_question2options
 
 # 加载文件
 file_name = '毛概_1'
 subject = file_name.split('_', 1)[0]
-path = r"/Users/steven/PycharmProjects/notion-create-exam-databace/origin_json_data" + file_name + ".json"
+path = r"/Users/steven/PycharmProjects/notion-create-exam-databace/origin_json_data/" + file_name + ".json"
 file = open(path, 'r', encoding='utf-8')
 
 # 创建 API 客户端
 # notion = Client(auth=os.environ['NOTION_TOKEN'])
-notion = Client(auth='secret_zjmsB5oeXEGw25ndV7JVN1j2C8EwE9jMbaUpGvVtZ6I')
+notion = NotionClient(auth='secret_zjmsB5oeXEGw25ndV7JVN1j2C8EwE9jMbaUpGvVtZ6I')
+
+# 创建redis_client
+redis_client = RedisBloomClient(host='localhost', port=6379, db=0)
 
 # 解析数据
 list_data = json.load(file)
 
 for data in list_data:
     stuQuestions = data["stuQuestions"]
+    index = 1
     for stuQuestion in stuQuestions:
+        print('执行第' + str(index) + '遍')
         stuQuestion_id = stuQuestion["id"]
         stuQuestion_title = stuQuestion["title"]
         stuQuestion_answer = stuQuestion["answer"]
@@ -35,55 +41,56 @@ for data in list_data:
         stuQuestion_options = stuQuestion["options"]
         stuQuestion_options_done = stu_question2options(stuQuestion_options)
 
+        if redis_client.bfExists('id', stuQuestion_id):
+            print('已存在的id:', stuQuestion_id)
+            index += 1
+            continue
+
         # 写入数据库
         notion.pages.create(
             **{
                 'parent': {'database_id': 'f406642b43ba49ff99844e621608f8d3'},
                 'properties': {
-                    'id': {
+                    'Id': {
                         'title': [
                             {
                                 'text': {
-                                    'content': stuQuestion_id  # 野菜名を指定
+                                    'content': stuQuestion_id
                                 }
                             }
                         ]
                     },
-                    'title': {
+                    'Title': {
                         'rich_text': [{
-                            'plain_text': stuQuestion_title,
                             'text': {
                                 'content': stuQuestion_title,
                                 'link': None
-                            },
-                            'type': 'text'
-                        }],
+                            }
+                        }]
                     },
-                    'answer': {
+                    'Answer': {
                         'rich_text': [{
-                            'plain_text': stuQuestion_answer,
                             'text': {
                                 'content': stuQuestion_answer,
                                 'link': None
                             },
-                            'type': 'text'
-                        }],
-                        'type': 'rich_text'
+                        }]
                     },
-                    'options': {
+                    'Options': {
                         'multi_select': stuQuestion_options_done,
-                        'type': 'multi_select'
                     },
-                    'questionType': {
+                    'QuestionType': {
                         'number': stuQuestion_type,
-                        'type': 'number'
                     },
-                    'subject': {
+                    'Subject': {
                         'select': {
                             'name': subject
                         },
-                        'type': 'select'
-                    },
+                    }
                 }
             }
         )
+        index += 1
+
+        # 把id写入redis布隆过滤器
+        redis_client.bfAdd('id', stuQuestion_id)
