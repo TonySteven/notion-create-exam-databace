f92bb12e494ac393e7eeeb9cec945ae9524f107b
StevenL <1012987522@qq.com>
2022-07-13 16:49:45 +0800

    feature: 适配问题类型8的存储.

--- analysis.py
+++ analysis.py
@@ -14,7 +14,7 @@ from redisbloom.client import Client as RedisBloomClient
 from utils.utils import stu_question2options
 
 # 加载文件
-file_name = '毛概_1'
+file_name = '英语_2'
 subject = file_name.split('_', 1)[0]
 path = r"/Users/steven/PycharmProjects/notion-create-exam-databace/origin_json_data/" + file_name + ".json"
 file = open(path, 'r', encoding='utf-8')
@@ -34,12 +34,28 @@ for data in list_data:
     index = 1
     for stuQuestion in stuQuestions:
         print('执行第' + str(index) + '遍')
-        stuQuestion_id = stuQuestion["id"]
-        stuQuestion_title = stuQuestion["title"]
-        stuQuestion_answer = stuQuestion["answer"]
         stuQuestion_type = stuQuestion["questionType"]
-        stuQuestion_options = stuQuestion["options"]
-        stuQuestion_options_done = stu_question2options(stuQuestion_options)
+        print('问题类型:' + str(stuQuestion_type))
+        # switchCase = switchCase()
+        # switchCase.case_to_function(stuQuestion_type)
+
+        stuQuestion_id = stuQuestion["id"]
+        stuQuestion_title = stuQuestion["title"][0:1000]
+        stuQuestion_answer = stuQuestion["answer"]
+
+        stuQuestion_options_done = []
+        # 单选和多选题直接获取选项
+        if stuQuestion_type == 1 or stuQuestion_type == 2:
+            stuQuestion_options = stuQuestion["options"]
+            stuQuestion_options_done = stu_question2options(stuQuestion_options)
+
+        # 如果是阅读理解有子题目,则获取子题目答案.
+        if stuQuestion_type == 8:
+            stuSubQuestions = stuQuestion["stuSubQuestion"]
+            stuQuestion_answer = []
+            for stuSubQuestion in stuSubQuestions:
+                stuSubQuestion_answer = stuSubQuestion["answer"]
+                stuQuestion_answer.append(stuSubQuestion_answer)
 
         if redis_client.bfExists('id', stuQuestion_id):
             print('已存在的id:', stuQuestion_id)
@@ -71,7 +87,7 @@ for data in list_data:
                     'Answer': {
                         'rich_text': [{
                             'text': {
-                                'content': stuQuestion_answer,
+                                'content': str(stuQuestion_answer),
                                 'link': None
                             },
                         }]
